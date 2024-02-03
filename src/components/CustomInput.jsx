42cd3ebd3a246442bfb52899e238056143f2c1f5
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-03 23:45:22 +0700

    #code

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -1,14 +1,14 @@
-import {TextInput} from 'react-native';
-import React, {forwardRef} from 'react';
+import { TextInput } from 'react-native';
+import React, { forwardRef } from 'react';
 
 const CustomInput = forwardRef((props, ref) => {
-  const {type, placeHolder} = props;
+  const { type, placeHolder } = props;
   return (
     <TextInput
       placeholderTextColor={'black'}
       placeholder={placeHolder}
       secureTextEntry={type === 'password' ? true : false}
-      style={{color: 'black'}}
+      style={{ color: 'black' }}
       ref={ref}
     />
   );
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,18 +1,17 @@
 /* eslint-disable prettier/prettier */
-import React, {useRef} from 'react';
-import {Text, TextInput, View} from 'react-native';
+import React, { useRef } from 'react';
+import { Text, TextInput, View } from 'react-native';
 import CustomInput from '../components/CustomInput';
 
-const HomeScreen = props => {
-  
-  const {user} = props;
+const HomeScreen = (props) => {
+  const { user } = props;
   const input = useRef(null);
 
   const inputProps = {
     type: 'name',
     placeHolder: 'Enter your name',
   };
-  
+
   return (
     <View
       style={{
@@ -20,9 +19,10 @@ const HomeScreen = props => {
         flex: 1,
         paddingHorizontal: 20,
         paddingVertical: 20,
-      }}>
+      }}
+    >
       <View>
-        <Text style={{color: 'black'}}>Hello {user}</Text>
+        <Text style={{ color: 'black' }}>Hello {user}</Text>
         <CustomInput ref={input} {...inputProps} />
       </View>
     </View>

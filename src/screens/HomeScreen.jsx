f540fb48a9dcacb38dccf1104f13c4831a54aa90
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-03 14:45:11 +0700

    #code

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 const HomeScreen = props => {
   const {user} = props;
-  return <Text>Hello {user}</Text>;
+  return (
+    <View style={{backgroundColor: 'white', flex: 1, paddingHorizontal: 20, paddingVertical: 20}}>
+      <Text style={{color: 'black'}}>Hello {user}</Text>
+    </View>
+  );
 };
 
 export default HomeScreen;

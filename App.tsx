bc1c125ae822d144210e5aad9d029a093982058e
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-04 22:11:43 +0700

    #code

--- App.tsx
+++ App.tsx
@@ -8,25 +8,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import Home from './src/screens/Home';
-import Account from './src/screens/Account';
-
-const Stack = createNativeStackNavigator();
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import NavBar from './src/navigation/NavBar';
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ headerStyle: { backgroundColor: '#fdf6e2' } }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Account" component={Account} />
-      </Stack.Navigator>
+      <NavBar />
     </NavigationContainer>
   );
 }
--- src/navigation/NavBar.jsx
+++ src/navigation/NavBar.jsx
@@ -2,14 +2,32 @@ import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/Home';
 import Account from '../screens/Account';
+import ADicons from 'react-native-vector-icons/AntDesign';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 const NavBar = (props) => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Account" component={Account} />
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <ADicons name="home" size={24} color={'black'} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" size={24} color={'black'} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };

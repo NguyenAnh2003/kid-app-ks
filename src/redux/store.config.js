50040fdd619d22caca9095719c5a9aef59f30cab
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-15 14:14:05 +0700

    #code

--- src/redux/store.config.js
+++ src/redux/store.config.js
@@ -4,6 +4,10 @@ import { persistReducer, persistStore } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { thunk } from 'redux-thunk';
 
+/**
+ * @access https://jscrambler.com/blog/how-to-use-redux-persist-in-react-native-with-asyncstorage
+ */
+
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,

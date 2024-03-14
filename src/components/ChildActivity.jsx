ec49c94a469adecdc8911212dbf5a1f4cf14ae6c
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-03-14 21:29:37 +0700

    #ui setup

--- src/components/ChildActivity.jsx
+++ src/components/ChildActivity.jsx
@@ -1,18 +1,23 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-const ChildActivity = React.memo(({childId, appName, appUsageTime, Date}) => {
+const ChildActivity = React.memo(({childId, appName, appUsageTime, date}) => {
   
-  /** activity data
+  /** activity data 
+   * @description child activity will be described with childId, application (app)
+   *  app icon, app usage time, and finally date they use this activity
    * @param childId
-   * @param application name
-   * @param app time used
-   * @param Date
+   * @param applicationName - appIcon (how to process?)
+   * @param appUsageTime
+   * @param date
    */
 
   return (
     <View>
-      <Text>ChildActivity</Text>
+      <Text style={{color: 'black'}}>ChidId {childId}</Text>
+      <Text style={{color: 'black'}}>appName {appName}</Text>
+      <Text style={{color: 'black'}}>usage {appUsageTime}</Text>
+      <Text style={{color: 'black'}}>{date}</Text>
     </View>
   );
 });

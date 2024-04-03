a32e55a2aa1455c5331ab99369cb1c1738667f28
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-04-03 13:15:06 +0700

    #code

--- App.tsx
+++ App.tsx
@@ -1,18 +1,10 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./src/screens/LoginScreen";
-import Register from "./src/screens/RegisterScreen";
-import ForgotPasswordInputEmail from "./src/screens/ForgotPasswordInputEmail";
-import ForgotPasswordInstruction from "./src/screens/ForgotPasswordInstruction";
-import ForgotPasswordCreateNewP from "./src/screens/CreateNewPasswordScreen";
-import { View, Text, Pressable, TouchableOpacity } from "react-native";
-import { Session } from "@supabase/supabase-js";
-import { useState, useEffect } from "react";
-import { supabase } from "./src/libs/supabase";
-import Home from "./src/screens/Home";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-<<<<<<< HEAD
 import { NavigationContainer } from '@react-navigation/native';
 import React, { useEffect, useRef, useState } from 'react';
 import NavBar from './src/navigation/NavBar';
@@ -58,33 +50,11 @@ function App(): React.JSX.Element {
         nextAppState === 'active'
       ) {
         console.log('App come to foreground and move to active');
-=======
-const Stack = createNativeStackNavigator();
-
-const App = () => {
-  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    const fetchSession = async () => {
-      const { data: session, error } = await supabase.auth.getSession();
-      if (session) {
-        setSession(session.session); // Extracting the 'session' property
->>>>>>> d5b78089dda1739b262efc7be6850d0343aebb1a
       } else {
-        setSession(null);
+        console.log(`Current state ${nextAppState}`);
       }
-      setHideSplashScreen(false);
-    };
-
-    fetchSession();
-
-    const authListener = supabase.auth.onAuthStateChange((_event, session) => {
-      if (session) {
-        setSession(session); // Extracting the 'session' property
-      } else {
-        setSession(null);
-      }
+      appState.current = nextAppState; //
+      setAppStateVisible(appState.current);
     });
 
     /** check permission */
@@ -98,42 +68,18 @@ const App = () => {
     checkPermission(); // permission isGranted
 
     return () => {
-      authListener.data.subscription.unsubscribe();
+      subscription.remove();
     };
   }, []);
 
   return (
-    <>
-      {hideSplashScreen ? null : (
+    <Provider store={configStore}>
+      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
         <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            {session ? (
-              <Stack.Screen name="Home">
-                {props => <Home {...props} session={session} />}
-              </Stack.Screen>
-            ) : (
-              <>
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Register" component={Register} />
-                <Stack.Screen
-                  name="ForgotPasswordInputEmail"
-                  component={ForgotPasswordInputEmail}
-                />
-                <Stack.Screen
-                  name="ForgotPasswordInstruction"
-                  component={ForgotPasswordInstruction}
-                />
-                <Stack.Screen
-                  name="ForgotPasswordCreateNewP"
-                  component={ForgotPasswordCreateNewP}
-                />
-              </>
-            )}
-          </Stack.Navigator>
+          <NavBar />
         </NavigationContainer>
-      )}
-    </>
+      </PersistGate>
+    </Provider>
   );
-};
-
+}
 export default App;

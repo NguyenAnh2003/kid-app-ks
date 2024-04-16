8d42fdee77310bb9b229a4a4e7cd3a1280f2660f
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-04-16 08:44:25 +0700

    #code

--- src/libs/supabase/file.services.js
+++ src/libs/supabase/file.services.js
@@ -1,6 +1,9 @@
 import { appStorage } from './supabase';
 
 export const uploadImage = async (file) => {
+  /** 
+   * @param file - used for uploading file to bucket
+   */
   try {
     const { data } = await appStorage.upload('public', file);
     if (data) return data;

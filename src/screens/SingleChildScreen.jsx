d14b6191b40c2966db0dc2b5cbf03bad8ea49e78
Sang <phunganhsang123@gmail.com>
2024-04-23 22:01:25 +0700

    add comment week-day

--- src/screens/SingleChildScreen.jsx
+++ src/screens/SingleChildScreen.jsx
@@ -75,9 +75,12 @@ const packageList = [
 // đây là số lương ngày và tuần muốn hiển thị, 
 // ví dụ numDay = 1 thì hiển thị thống kê trong vòng 1 ngày trước,
 // numDay = 2 thì hiển thị thống kê trong vòng 2 ngày trước
-
 const numDay = 1
 const numWeek = 1
+
+// t nghĩ cái ni nên bỏ cái trượt như caurosel xong cho coi 
+// nhiều ngày hay nhiều tuần đó để coi được nhiều hơn
+// mấy cái caurosel với giao diện nớ thì t hơi lỏ :v
 
 const SingleChildScreen = ({ route, navigation }) => {
   /**

c5daebbbbad103029724b579070b075db73d0bdf
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-05-16 11:45:03 +0700

    #code

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,8 +1,15 @@
 /* eslint-disable prettier/prettier */
-import React, { useEffect, useReducer, useRef, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from 'react';
 import {
   Button,
   NativeModules,
+  RefreshControl,
   ScrollView,
   StyleSheet,
   Text,
@@ -35,34 +42,6 @@ const styles = StyleSheet.create({
   },
 });
 
-const childList = [
-  /** childId, childName, ChildAvatar */
-  {
-    childId: '7ce14780-1a03-438a-9596-973d81725fa7',
-    childName: 'Nguyen Anh',
-    childPNumber: '0358423237',
-    avatarUrl:
-      'https://scontent.fdad4-1.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG9i1qn6l43gkgamWlIFfcBso2H55p0AlGyjYfnmnQCUWkluqxxNCGvGVG1PVYHXNR6aK5qtqLm_qilNbC_bMV0&_nc_ohc=19M80UHL7OUAX-2OV9R&_nc_ht=scontent.fdad4-1.fna&oh=00_AfD76oKUeKfB5Wu8k0a_3MdD4Sou2SzL54HRcJ17vFGTlA&oe=6624F0B8',
-    phoneType: 'Sam Sung',
-  },
-  {
-    childId: '1baf7534-f582-403f-a5ef-f09464b5733e',
-    childName: 'Bin Bin',
-    childPNumber: '0358423237',
-    avatarUrl:
-      'https://scontent.fdad4-1.fna.fbcdn.net/v/t39.30808-1/416167704_2320873964969530_3091803937576343392_n.jpg?stp=dst-jpg_s200x200&_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFXUgMaHDLADOcW1y8ihn69FjXwQ_1c8q4WNfBD_VzyrszQ0F2EhicA5IfGKJ_2MwOgAi3W8hVCLN4vAF34CrrF&_nc_ohc=c_IFlPPF8gUAX_s3ex-&_nc_ht=scontent.fdad4-1.fna&oh=00_AfCUdFhSbBJ4__9Xa99Ct5hWJz3N9JJ33g7362OFilnYDQ&oe=6602BBC1',
-    phoneType: 'Sam Sung',
-  },
-  {
-    childId: '7ce14780-1a03-438a-9596-973d81725fa0',
-    childName: 'Bin Bin',
-    childPNumber: '0358423237',
-    avatarUrl:
-      'https://scontent.fdad4-1.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG9i1qn6l43gkgamWlIFfcBso2H55p0AlGyjYfnmnQCUWkluqxxNCGvGVG1PVYHXNR6aK5qtqLm_qilNbC_bMV0&_nc_ohc=19M80UHL7OUAX-2OV9R&_nc_ht=scontent.fdad4-1.fna&oh=00_AfD76oKUeKfB5Wu8k0a_3MdD4Sou2SzL54HRcJ17vFGTlA&oe=6624F0B8',
-    phoneType: 'Sam Sung',
-  },
-];
-
 const reducer = (state, action) => {
   switch (action.type) {
     case 'CHILD_LIST':
@@ -87,6 +66,8 @@ const HomeScreen = ({ user, navigation, route }) => {
     children: [],
     isFetching: true,
   });
+  /** refresh */
+  const [refresh, setRefresh] = useState(false);
 
   const currentUserSession = useSelector((state) => state.userReducers?.user);
 
@@ -106,45 +87,63 @@ const HomeScreen = ({ user, navigation, route }) => {
     }
   }, []);
 
+  const onRefresh = useCallback(() => {
+    setRefresh(true);
+    const fetchDataaa = async () => {
+      const data = await getChildInfo(childId);
+      if (data) dispatch({ type: 'CHILD_LIST', payload: data });
+    };
+
+    fetchDataaa();
+    setRefresh(false);
+  }, []);
+
   return state.isFetching ? (
     <SplashScreen />
   ) : (
-    <View style={[globalStyle.container, { flex: 1, paddingHorizontal: 10 }]}>
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <View style={{ flexDirection: 'column', gap: 10, width: '100%' }}>
-          {/** list of child */}
-          {state &&
-            state.children.map((i, index) => (
-              <TouchableOpacity
-                key={i.childId}
-                onPress={() =>
-                  navigation.navigate('SingleChild', {
-                    childId: i.id,
-                    childName: i.kidName,
-                    childImage: i.avatarUrl,
-                    phoneType: i.phoneType,
-                  })
-                }
-              >
-                <ChildCard
-                  key={i.id}
-                  childName={i.kidName}
-                  childPhoneNumber={i.phone}
-                  childAvatar={i.avatarUrl}
-                  phoneType={i.phoneType}
-                />
-              </TouchableOpacity>
-            ))}
-        </View>
-        {/** create child button */}
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('AddChild')}
-        >
-          <AntDesign name="pluscircleo" size={30} color={'white'} />
-        </TouchableOpacity>
-      </ScrollView>
-    </View>
+    <ScrollView
+      contentContainerStyle={{ flex: 1 }}
+      refreshControl={
+        <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+      }
+    >
+      <View style={[globalStyle.container, { flex: 1, paddingHorizontal: 10 }]}>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <View style={{ flexDirection: 'column', gap: 10, width: '100%' }}>
+            {/** list of child */}
+            {state &&
+              state.children.map((i, index) => (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() =>
+                    navigation.navigate('SingleChild', {
+                      childId: i.id,
+                      childName: i.kidName,
+                      childImage: i.avatarUrl,
+                      phoneType: i.phoneType,
+                    })
+                  }
+                >
+                  <ChildCard
+                    key={i.id}
+                    childName={i.kidName}
+                    childPhoneNumber={i.phone}
+                    childAvatar={i.avatarUrl}
+                    phoneType={i.phoneType}
+                  />
+                </TouchableOpacity>
+              ))}
+          </View>
+          {/** create child button */}
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('AddChild')}
+          >
+            <AntDesign name="pluscircleo" size={30} color={'white'} />
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </ScrollView>
   );
 };
 
